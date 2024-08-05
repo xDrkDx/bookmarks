4a198a90f8cf04c0b9da84fc7b8f902e9f3cb07d
xDrkDx <venger989@gmail.com>
2024-08-05 14:46:06 +0300

    init2

--- bookmarks/images/urls.py
+++ bookmarks/images/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = "images"
-
+#TEST
 urlpatterns = [
     path("create/", views.image_create, name="create"),
     path("detail/<int:id>/<slug:slug>/", views.image_detail, name="detail"),
